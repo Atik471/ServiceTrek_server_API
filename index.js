5bd2127dc80741a7cd0c6fac043f916de49a023f
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-12-21 22:56:33 +0600

    added initial project setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('ServieTrek');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
